65d647137c464d3d423c9f59cbe5d80fda7d2633
Eric <ericpaulbishop@gmail.com>
2010-05-08 19:53:33 -0400

    allow either wiki or repository landing page for single projects

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ ActionController::Routing::Routes.draw do |map|
 	if(defined?(projects.length))
 		if(projects.length == 1)
 			id=projects[0]
-			map.connect '', :controller=> 'repositories', :id=>id, :action=>'show'
+			if(settings['single_landing_page'] == 'repository')
+				map.connect '', :controller=> 'repositories', :id=>id, :action=>'show'
+			else
+				map.connect '', :controller=> 'wiki', :id=>id, :action=>'show'
+			end
 		end
 	end
 	
--- init.rb
+++ init.rb
@@ -4,8 +4,11 @@ require 'redmine'
 RAILS_DEFAULT_LOGGER.info 'Starting Single Project plugin for RedMine'
 
 Redmine::Plugin.register :single_project_plugin do
-  name 'Single Project plugin'
-  author 'Eric Bishop'
-  description 'Hides multi-project display when only when project is present'
-  version '1.0.0'
+	name 'Single Project plugin'
+	author 'Eric Bishop'
+	description 'Hides multi-project display when only when project is present'
+	version '1.0.0'
+	settings :default => {
+    			'single_landing_page' => 'wiki' 
+    			}
 end
