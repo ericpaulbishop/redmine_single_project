f9aaed99dd44a539e3e03a4f0b284793e5fa062c
Eric <ericpaulbishop@gmail.com>
2010-05-10 15:34:17 -0400

    fix typo

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ RAILS_DEFAULT_LOGGER.info 'Starting Single Project plugin for RedMine'
 Redmine::Plugin.register :redmine_single_project do
 	name 'Single Project plugin'
 	author 'Eric Bishop'
-	description 'Hides multi-project display when only when project is present'
+	description 'Hides multi-project display when only one project is present'
 	version '1.0.0'
 	settings :default => {
     			'single_landing_page' => 'repository' 
