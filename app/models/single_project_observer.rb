24502d3c110b664f21e2ec9f9186d18c8ad3e9be
Eric Bishop <ericpaulbishop@gmail.com>
2011-04-21 16:37:17 -0400

    make single project display kick in again if we delete a project and go back to having only a single project

--- app/models/single_project_observer.rb
+++ app/models/single_project_observer.rb
@@ -2,14 +2,27 @@ class SingleProjectObserver < ActiveRecord::Observer
 	observe :project
 	
 	def after_create(object)
+		update_routes_and_menus object
+	end
+
+	def after_destroy(object)
+		update_routes_and_menus object
+	end
+
+	protected
+	def update_routes_and_menus(object)
+		# only update if num projects = 1 (may have just deleted a project, now single again)
+		# or 2 (may have just added a project, no longer single)
+		# otherwise, nothing changes
 		if object.is_a?(Project)
 			all_projects = Project.find(:all)
-			if(all_projects.length > 1)
-				ActionController::Routing::Routes.draw do |map|
-					map.home '', :controller => 'welcome'
-				end
+			if all_projects.length == 1 || all_projects.length == 2
+				ActionController::Routing::Routes.reload!
 				Redmine::MenuManager.map :top_menu do |menu|
-					menu.push :projects, { :controller => 'projects', :action => 'index', :after =>:my_page }, :caption => :label_project_plural
+					menu.delete :projects rescue nil
+					if all_projects.length > 1
+						menu.push  :projects, { :controller => 'projects', :action => 'index', :after =>:my_page }, :caption => :label_project_plural
+					end
 				end
 			end
 		end
