0c31927b814b155e6385541dbba5faa7343bb66e
Eric Bishop <ericpaulbishop@gmail.com>
2011-04-21 17:10:57 -0400

    fix wiki display

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ ActionController::Routing::Routes.draw do |map|
 	projects = Project.find(:all, :order => 'lft')
 	if(defined?(projects.length))
 		if(projects.length == 1)
-			id=projects[0]
+			id=(projects[0]).identifier
 			if(Setting.plugin_redmine_single_project['single_landing_page'] == 'repository')
 				map.connect '', :controller=> 'repositories', :id=>id, :action=>'show'
 			else
-				map.connect '', :controller=> 'wiki', :id=>id, :action=>'show'
+				map.connect '', :controller=> 'wiki', :project_id=>id, :action=>'show'
 			end
 		end
 	end
